baaa321d3366dd921436c64a52aeb79b8b6726f6
writeriks <emirhaktan@gmail.com>
2024-12-03 17:51:49 +0100

    update api fetching

--- services/api-service/api-service.ts
+++ services/api-service/api-service.ts
@@ -2,10 +2,12 @@ import { AssetPrice } from '@/types/asset-type';
 import { CoinMarketCapitalData } from '@/types/coin-marketcap-types';
 import { FearAndGreed } from '@/types/fear-and-greed';
 
-export const fetchUrl = async (url: string, payload?: {}): Promise<Response> => {
+export const fetchUrl = async (url: string, headers?: {}, payload?: {}): Promise<Response> => {
   try {
     const response = await fetch(url, {
-      ...payload,
+      headers: headers,
+      next: { revalidate: 60 },
+      body: JSON.stringify(payload),
     });
 
     return response;
@@ -23,22 +25,18 @@ export const getAssetPrice = async (ticker: string): Promise<AssetPrice> => {
 };
 
 export const getFearAndGreedIndex = async (): Promise<FearAndGreed> => {
-  const response = await fetchUrl('https://api.alternative.me/fng/?limit=1', {
-    next: { revalidate: 60 },
-  });
+  const response = await fetchUrl('https://api.alternative.me/fng/?limit=1');
 
   const data = await response.json();
   return data.data[0];
 };
 
 export const fetchMarketData = async (): Promise<CoinMarketCapitalData> => {
+  const headers = { 'X-CMC_PRO_API_KEY': `${process.env.NEXT_PUBLIC_COINMARKETCAP_API_KEY}` };
+
   const response = await fetchUrl(
     'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest',
-    {
-      headers: {
-        'X-CMC_PRO_API_KEY': `${process.env.NEXT_PUBLIC_COINMARKETCAP_API_KEY}`,
-      },
-    }
+    headers
   );
   const { data } = await response.json();
   return data;
