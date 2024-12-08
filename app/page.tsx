14d5cf129a250baa9f0f897b695c8290812ca58b
writeriks <emirhaktan@gmail.com>
2024-12-08 22:36:36 +0100

    move force dynamic to page level

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Metrics from '@/components/metrics';
 import CryptoAnalyzer from '@/components/crypto-analyzer/crypto-analyzer';
 
+export const dynamic = 'force-dynamic';
+
 const Home: React.FC = async () => (
   <main className='container mx-auto sm:p-4 py-2 space-y-6'>
     <Metrics />
